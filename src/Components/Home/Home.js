efaa1e20581237f543e1bad5eab8763f516e7cb9
Shahidul Islam <shahidulislam7150@gmail.com>
2022-06-09 09:08:07 +0600

    add hero section

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Hero from './Hero';
 
 const Home = () => {
     return (
         <div>
-            <h1 className='text-3xl'>Hello</h1>
+            <Hero></Hero>
         </div>
     );
 };
